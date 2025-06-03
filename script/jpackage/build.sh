389367b125aef648b3f59e6f96d32b5aee65639a
Yeding <kanaumachi@gmail.com>
2025-06-03 23:38:36 +0900

    build(jpackage): 更新构建脚本

--- script/jpackage/build.sh
+++ script/jpackage/build.sh
@@ -14,7 +14,7 @@ DIR=$(cd "$(dirname "$0")/../../" && pwd)
 SCRIPT_DIR=$(cd "$(dirname "$0")" && pwd)
 MODULES="$DIR/target/build"
 ICON="$DIR/images/icons/cat.icns"
-TARGET_DIR="$SCRIPT_DIR/LabelPlusFX"
+TARGET_DIR="$SCRIPT_DIR/LabelPlusFX.app"
 OUTPUT_DIR="$SCRIPT_DIR/Output"
 ZIP_NAME="LabelPlusFX-$VERSION-Mac.zip"
 
@@ -35,14 +35,12 @@ jpackage --verbose \
     --main-jar "lpfx-$VERSION.jar" \
     --main-class ink.meodinger.lpfx.LauncherKt
 
-# 验证是否生成了 LabelPlusFX 文件夹
-if [ ! -d "$TARGET_DIR" ]; then
-    echo "[ERROR] Application folder not generated at: $TARGET_DIR"
-    exit 1
+# 验证是否真的生成了内容
+if [ "$(ls -A "$TARGET_DIR")" ]; then
+    echo "[INFO] Application folder is NOT empty."
 else
-    echo "[INFO] Application folder generated successfully."
-    echo "[INFO] Directory structure:"
-    ls -1 "$TARGET_DIR"
+    echo "[ERROR] Application folder is empty!" >&2
+    exit 1
 fi
 
 # 拷贝额外资源文件
@@ -50,7 +48,7 @@ FILE_LIST="LabelPlusFXDict.alias"
 
 for file in $FILE_LIST; do
     if [ -f "$SCRIPT_DIR/$file" ]; then
-        cp -f "$SCRIPT_DIR/$file" "$TARGET_DIR/"
+        cp -f "$SCRIPT_DIR/$file" "$TARGET_DIR/Contents/Resources/"
         echo "[Success] Copied: $file"
     else
         echo "[Warning] Missing: $file (skipped)"
@@ -68,7 +66,7 @@ fi
 # 打包成 ZIP
 echo "Packing into ZIP: $ZIP_NAME"
 cd "$SCRIPT_DIR" || exit
-zip -r "$OUTPUT_DIR/$ZIP_NAME" "LabelPlusFX"
+zip -r "$OUTPUT_DIR/$ZIP_NAME" "LabelPlusFX.app"
 
 # 验证 ZIP 是否存在
 if [ -f "$OUTPUT_DIR/$ZIP_NAME" ]; then
